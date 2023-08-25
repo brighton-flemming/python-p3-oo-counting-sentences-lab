2dd74789a8650f7457220943e2c3995d87612bdf
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-25 10:21:01 +0300

    Mission Passed

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value = ""):
+     if not isinstance(value, str):
+          print("The value must be a string.")
+          self._value = None
+     elif value == "":
+           print("The value must be a string.")
+           self._value = None
+     else:
+          self._value = value
+
+  def __str__(self) -> str:
+        pass
+        
+     
+         
+
+
+
+  def is_sentence():
+     pass
+  
+try:
+    my_instance = MyString()
+    print(my_instance._value)
+
+    my_instance = MyString("Hello, world!")
+    print(my_instance._value)
+
+    invalid_instance = MyString(42) 
+except ValueError as e:
+    print(e) 
+
+
+ 
